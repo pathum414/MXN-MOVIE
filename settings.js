eec914f9db7a943af0d430fa2ee73c6cc1828b07
pathum414 <chinthakapathum863@gmail.com>
2025-04-02 13:40:48 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {    
-SESSION_ID: process.env.SESSION_ID || 'MXN-MOVIE=UStmWICC#6dWz30Op0BqE08BkBfV1Zmw2j-s5shIMr1_i8oBGdH8',
+SESSION_ID: process.env.SESSION_ID || 'MXN-MOVIE=4T9kxJKR#rZ5Y_KKkn1pyAb0nPWbw5Dv07VOoWQP2FDSJXnV9voo',
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
 PREFIX:  process.env.PREFIX || ['.'] ,
 DIRECTION: true     
